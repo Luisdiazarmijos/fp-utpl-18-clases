5a02b899485c90f86924ba88bd1bdc8bb87c374f
Luis Díaz <lucho2911diaz@gmail.com>
2018-05-17 23:29:59 -0500

    trabajo en clase

---
+++ clase_17_05_2018/ciclo_for/src/ciclo_for/CLASE3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclo_for;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CLASE3 {
+    
+    public static void main(String args[]) {
+        // TODO code application logic here
+    
+    for (int contador = 1; contador <=10; contador++){
+        if (contador % 2==0) {
+        System.out.println("");
+            
+        }
+            System.out.printf("%d " , contador);
+}
+            System.out.println("");
+}
+
+}
+
